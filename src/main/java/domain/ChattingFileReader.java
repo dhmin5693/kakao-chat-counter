7101e54fbd73cdeaf20c1b25651228eb7ce3f61c
dhmin5693 <dhmin5693@naver.com>
2021-04-11 23:04:34 +0900

    feat: file read / error(illegal file path)

--- src/main/java/domain/ChattingFileReader.java
+++ src/main/java/domain/ChattingFileReader.java
@@ -1,23 +1,27 @@
 package domain;
 
+import exception.IllegalFilePathException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ChattingFileReader {
 
-    public ChattingFileReader(String path) {
+    private final String filePath;
 
+    public ChattingFileReader(String filePath) {
+        this.filePath = filePath;
     }
 
-    public void foo() {
+    public Stream<String> getStreamData() {
 
         // do something...
 
-//        String filePath = "";
-
-//        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-//        try {
-//            Stream<String> stream = Files.lines(Paths.get(filePath));
-//            stream.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            return Files.lines(Paths.get(filePath));
+        } catch (IOException e) {
+            throw new IllegalFilePathException(filePath);
+        }
     }
 }
--- src/test/java/domain/ChattingFileReaderTest.java
+++ src/test/java/domain/ChattingFileReaderTest.java
@@ -1,11 +1,12 @@
 package domain;
 
-import java.io.FileNotFoundException;
+import exception.IllegalFilePathException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ChattingFileReaderTest {
@@ -26,7 +27,17 @@ class ChattingFileReaderTest {
         "src/test/resources/mac-error-01.txt"
     })
     @ParameterizedTest
-    void fail(String path) {
-        assertThrows(FileNotFoundException.class, () -> new ChattingFileReader(path));
+    void fail01(String path) {
+
+        var exception = assertThrows(IllegalFilePathException.class, () -> {
+            var fileReader = new ChattingFileReader(path);
+            fileReader.getStreamData();
+        });
+
+        var actual = exception.getMessage();
+        var expected = new IllegalFilePathException(path).getMessage();
+
+        assertEquals(actual, expected);
     }
+
 }
