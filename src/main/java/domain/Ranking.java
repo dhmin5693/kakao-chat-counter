61b12d1b99b764abfc99878650efa9696e0ddd8e
dhmin5693 <dhmin5693@naver.com>
2021-04-13 01:35:43 +0900

    feat: print rank number

--- src/main/java/domain/Ranking.java
+++ src/main/java/domain/Ranking.java
@@ -2,11 +2,14 @@ package domain;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toUnmodifiableList;
 
 public class Ranking {
+
+    private static final int INITIAL_RANK = 1;
 
     private final List<User> users;
 
@@ -18,8 +21,11 @@ public class Ranking {
 
     @Override
     public String toString() {
+
+        var rankNumberCounter = new AtomicInteger(INITIAL_RANK);
+
         return users.stream()
-                    .map(User::toString)
+                    .map(user -> rankNumberCounter.getAndIncrement() + "위 - " + user.toString())
                     .collect(joining("\n"));
     }
 
